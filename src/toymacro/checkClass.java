1f0f2d120a7383524aa8881473a5e078c69c10c6
ParkYoungDuk <dydejrdms@naver.com>
2023-08-17 14:57:26 +0900

    checkClass에 Macro 재료들 넣어놨으니 챙기기

--- src/toymacro/checkClass.java
+++ src/toymacro/checkClass.java
@@ -5,10 +5,16 @@
  */
 package toymacro;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
 import java.awt.Robot;
-import java.awt.event.InputEvent;
-import java.awt.event.KeyEvent;
+import java.awt.Toolkit;
+import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.WindowConstants;
 
 public class checkClass {
     public static void main(String[] args) {
@@ -22,13 +28,58 @@ public class checkClass {
             //우측 하단에 있는거
             //r.mouseMove(1090, 800);
             
-            r.mouseMove(850, 680);
+            r.mouseMove(1900, 680);
             //r.mouseMove(1800, 830);
             
-            //31
             
-        } catch (Exception e) {
-            System.err.println(e);
-        }
+            //전체해상도 구하기
+            Dimension res = Toolkit.getDefaultToolkit().getScreenSize();
+            System.out.println("해상도 : " + res.width + " x " + res.height);  
+            
+            //포인트 지점 구하기
+//            PointerInfo pt = MouseInfo.getPointerInfo();
+//            while(true) {
+//                    pt = MouseInfo.getPointerInfo();
+//                    System.out.println(pt.getLocation()); // x,y 
+//                    System.out.println(pt.getLocation().x);// x 좌표
+//                    System.out.println(pt.getLocation().y); // y 좌표
+//            }
+            
+            JFrame frame = new JFrame("Click Coordinates Example");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+            JPanel panel = new JPanel() {
+                @Override
+                public void paintComponent(Graphics g) {
+                    super.paintComponent(g);
+                }
+            };
+
+            panel.addMouseListener(new MouseAdapter() {
+                @Override
+                public void mouseClicked(MouseEvent e) {
+                    int x = e.getX();
+                    int y = e.getY();
+                    System.out.println("Clicked at coordinates: (" + x + ", " + y + ")");
+                    frame.setVisible(false);
+                    
+                    //frame.setDefaultCloseOperation();
+                }
+            });
+            
+            
+            
+            frame.add(panel);
+            //전체화면
+            frame.setSize(res.width, res.height);
+            //전체화면은 아닌데 그런느낌
+            frame.setUndecorated(true);
+            frame.setBackground(new Color(0,0,0,122));
+            panel.setBackground(new Color(0,0,0,122));
+            frame.setVisible(true);
+
+            } catch (Exception e) {
+                System.err.println(e);
+            }
     }
 }
